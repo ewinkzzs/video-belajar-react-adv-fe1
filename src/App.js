b9fd509a326bcc34b6fe30524c8d4e1c0c80a446
Erwin Riyanto <ewinkzzs@gmail.com>
2025-07-06 14:40:30 +0800

    add project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/beranda" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
